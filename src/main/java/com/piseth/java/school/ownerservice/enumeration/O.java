64029a9dee49c690357610562f0d61744eaf2b67
Kosal Nim <kosall123nim@gmail.com>
2026-02-20 05:26:02 +0700

    create table using liquibase

--- src/main/java/com/piseth/java/school/ownerservice/enumeration/O.java
+++ src/main/java/com/piseth/java/school/ownerservice/enumeration/O.java
@@ -4,7 +4,19 @@ public class O {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		String name="Kosal Nim Kalar";
+		String reverse = reverse(name);
+		System.out.println(reverse);
 
+	}
+	private static String reverse(String name) {
+		String[] split = name.split(" ");
+		String n="";
+		for(int i=split.length-1;i>=0;i--) {
+			n+=split[i];
+			
+		}
+		return n;
 	}
 
 }
